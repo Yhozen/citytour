900cbaaa8f0edd860d5b89e8e6fa943e36f5addf
Gabriel <gabriel@garox.org>
2019-02-25 22:13:55 -0300

    Updates

---
+++ precache-manifest.797e0393358ad5a1c65789965bb93cd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "31bf0e9b39aa3c930fa1",
+    "url": "/citytour/js/about.77c93d06.js"
+  },
+  {
+    "revision": "88d8d820e64ae2ffc097",
+    "url": "/citytour/css/app.cb07f2e5.css"
+  },
+  {
+    "revision": "88d8d820e64ae2ffc097",
+    "url": "/citytour/js/app.f695cad5.js"
+  },
+  {
+    "revision": "138c47176586f9e9bc1e",
+    "url": "/citytour/js/chunk-vendors.2b72b177.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/citytour/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0dbda9616b0ae54d20cc072e129c3f42",
+    "url": "/citytour/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/citytour/robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a32879399e7939e29f9dae3ca457a0b.js"
+  "/citytour/precache-manifest.797e0393358ad5a1c65789965bb93cd8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "citytour"});
