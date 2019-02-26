f84296f2dab52ba85a3cc434bf261ac503e6a73a
Gabriel <gabriel@garox.org>
2019-02-25 22:05:33 -0300

    Updates

---
+++ precache-manifest.2a32879399e7939e29f9dae3ca457a0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "31bf0e9b39aa3c930fa1",
+    "url": "/js/about.77c93d06.js"
+  },
+  {
+    "revision": "34e678c308b959992d78",
+    "url": "/css/app.cb07f2e5.css"
+  },
+  {
+    "revision": "34e678c308b959992d78",
+    "url": "/js/app.c89fd70a.js"
+  },
+  {
+    "revision": "138c47176586f9e9bc1e",
+    "url": "/js/chunk-vendors.2b72b177.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "494732459287fdd6c83eebf5cc12090b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
